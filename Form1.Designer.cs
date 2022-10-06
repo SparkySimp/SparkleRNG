cf30aecc6d9967b4ffa3335d48f9c025f66936da
SparkySimp <ozturkyigitcemal@gmail.com>
2022-10-06 12:00:47 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace SparkleRNG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblEkran = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timerRNG = new System.Windows.Forms.Timer(this.components);
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(329, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Number Is:";
+            // 
+            // lblEkran
+            // 
+            this.lblEkran.AutoSize = true;
+            this.lblEkran.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEkran.Location = new System.Drawing.Point(21, 33);
+            this.lblEkran.Name = "lblEkran";
+            this.lblEkran.Size = new System.Drawing.Size(0, 73);
+            this.lblEkran.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // timerRNG
+            // 
+            this.timerRNG.Enabled = true;
+            this.timerRNG.Interval = 3000;
+            this.timerRNG.Tick += new System.EventHandler(this.timerRNG_Tick);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(655, 93);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(133, 13);
+            this.lblTimer.TabIndex = 2;
+            this.lblTimer.Text = "Next Number In 3 seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 115);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.lblEkran);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblEkran;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Timer timerRNG;
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SparkleRNG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        protected Random prng = new Random();
+        protected int secs = 3;
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (secs > 0)
+                secs--;
+            else
+                secs = 3;
+
+            lblTimer.Text = $"Next Number In {secs} seconds";
+        }
+
+        private void timerRNG_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            lblEkran.Text = prng.Next().ToString();
+            timer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
